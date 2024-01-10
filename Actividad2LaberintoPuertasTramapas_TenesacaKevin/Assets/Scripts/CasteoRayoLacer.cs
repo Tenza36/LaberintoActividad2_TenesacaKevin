b8290395f5a17be32cb3848c5d0071c916ee9780
Tenza36 <ktenesaca36@gmail.com>
2024-01-10 18:01:54 -0500

    Inicio2

--- Actividad2LaberintoPuertasTramapas_TenesacaKevin/Assets/Scripts/CasteoRayoLacer.cs
+++ Actividad2LaberintoPuertasTramapas_TenesacaKevin/Assets/Scripts/CasteoRayoLacer.cs
@@ -46,7 +46,7 @@ public class CasteoRayoLacer : MonoBehaviour
         laserLine.enabled = false;
     }
 }*/
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -97,6 +97,65 @@ public class CasteoRayoLacer : MonoBehaviour
                     abrirPuertas.OnTriggerExit(null);
                 }
 
+                laserLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * gunRange));
+            }
+            StartCoroutine(ShootLaser());
+        }
+    }
+
+    IEnumerator ShootLaser()
+    {
+        laserLine.enabled = true;
+        yield return new WaitForSeconds(laserDuration);
+        laserLine.enabled = false;
+    }
+}
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CasteoRayoLacer : MonoBehaviour
+{
+    public Camera playerCamera;
+    public Transform LaserOrigin;
+    public float gunRange = 50f;
+    public float fireRate = 0.2f;
+    public float laserDuration = 0.05f;
+
+    LineRenderer laserLine;
+    float fireTimer;
+
+    void Awake()
+    {
+        laserLine = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        fireTimer += Time.deltaTime;
+        if (Input.GetButtonDown("Fire1") && fireTimer > fireRate)
+        {
+            fireTimer = 0;
+
+            // Utiliza LaserOrigin.position como punto de inicio del rayo
+            Vector3 rayOrigin = LaserOrigin.position;
+
+            laserLine.SetPosition(0, rayOrigin);
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin, playerCamera.transform.forward, out hit, gunRange))
+            {
+                AbrirPuertas abrirPuertas = hit.transform.GetComponent<AbrirPuertas>();
+                if (abrirPuertas != null)
+                {
+                    abrirPuertas.OnTriggerEnter(hit.collider); // Pasa el collider relevante
+                }
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                // Si no hay colisión, puedes hacer algo aquí si es necesario
                 laserLine.SetPosition(1, rayOrigin + (playerCamera.transform.forward * gunRange));
             }
             StartCoroutine(ShootLaser());
